8961731b0714cb32b208da040fa7b96efa278f75
João Pedro <jcunhaamorim@gmail.com>
2025-07-30 22:07:31 -0300

    Create Exercicio_1.py

---
+++ Testes/Exercicio_1.py
@@ -0,0 +1,33 @@
+import statistics
+"""Soma de dois números inteiros
+Numero_1 = int(input("Insira um numero = "))
+Numero_2 = int(input("Insira outro numero = "))
+Soma = Numero_1 + Numero_2
+print("O resultado é igual a",Soma)"""
+
+""""Divisão por 5 de inteiros
+Div = int(input("Insira um numero = "))
+Resultado_divisao = Div/5
+print("O resultado é", Resultado_divisao)"""
+
+"""Divisão entre dois numeros inteiros
+Numero_1 = int(input("Insira um número: "))
+Numero_2 = int(input("Insira um número: "))
+Multiplica = Numero_1 * Numero_2
+print("O resultado é", Divisao)"""
+
+"""Calcular o quadrado de um numero inteiro
+Numero_1 = int(input("Informe um número: "))
+Calculo_square = Numero_1**2
+print("O resultado do cálculo é", Calculo_square)"""
+
+"""Soma de dois numeros flutuantes
+Numero_1 = float(input("Insira um numero = "))
+Numero_2 = float(input("Insira outro numero = "))
+Soma = Numero_1 + Numero_2
+print("O resultado é igual a",Soma)"""
+
+"""Cálculo da média de flutuantes
+Num_1 = float(input("Insira um numero = "))
+Num_2 = float(input("Insira outro numero = "))
+print(statistics.mean([Num_1,Num_2]))"""
