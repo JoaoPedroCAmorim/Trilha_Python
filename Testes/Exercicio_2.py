e7de4ba11005f7da8b65fd76f6d9a6a4a3d202da
João Pedro <jcunhaamorim@gmail.com>
2025-08-02 17:47:03 -0300

    Update Exercicio_2.py

---
+++ Testes/Exercicio_2.py
@@ -0,0 +1,37 @@
+""" Verifica temperatura
+try:
+    temperatura_celsius = float(input("Informe a temperatura: "))
+    fahrenheit = (temperatura_celsius * 9/5) + 32
+    print(f"{temperatura_celsius}°C é igual a {fahrenheit}°F.")
+except ValueError:
+    print("Por favor, digite um número válido para a temperatura.")"""
+
+""" Verifica um palindromo
+Frase = input("Insira uma frase/palavra: ")
+if isinstance(Frase,str):
+    formate = Frase.replace(" ", "").lower()
+    if formate == formate[::-1]:
+        print("É um palindromo.")
+    else:
+        print("Não é um palindromo.")
+else:
+    print("Entrada inválida. Digite uma palavra ou frase.")"""
+
+"""Calculadora simples
+try:
+    Valor_um = float(input("Insira um número: "))
+    Valor_dois = float(input("Insira um número: "))
+    Operador = input("Informa o operador = ")
+    if Operador == '+':
+        resultado = Valor_um + Valor_dois
+    elif Operador == '-':
+        resultado = Valor_um - Valor_dois
+    elif Operador == '*':
+        resultado = Valor_um * Valor_dois
+    elif Operador == '/':
+        resultado = Valor_um/Valor_dois
+    else:
+        print("Operador invalido ou divisão por zero.")
+    print("Resultado:", resultado)
+except ValueError:
+    print("Erro: Entrada inválida. Deve ser inserido números")"""
