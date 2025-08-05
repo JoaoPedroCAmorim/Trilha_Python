6f2160d6c66072b0de97af44b5cf7686bf52cea9
João Pedro <jcunhaamorim@gmail.com>
2025-08-04 22:36:49 -0300

    Create Exercicio_3.py

---
+++ Testes/Exercicio_3.py
@@ -0,0 +1,37 @@
+"""try:
+    QNTD: float = float(input("Informe a quantidade de vendas: "))
+    Preco: float = float(input("Informe o preço da venda: "))
+    if QNTD > 0 and Preco > 0:
+        print("Dados válidos")
+    else:
+        print("Dados inválidos")
+except ValueError:
+    print("Erro: Entrada inválida. Deve ser inserido números")
+    exit()
+Valor_venda: float = QNTD * Preco
+print("O valor vendido é", Valor_venda)"""
+
+"""try:
+    temperatura: float = float(input("Informe a temperatura atual: "))
+    if temperatura < 18:
+        print("A temperatura atual está baixa")
+    elif 18<= temperatura <= 26:
+        print("A temperatura atual é normal")
+    else:
+        print("A temperatura é atual está alta")
+except ValueError:
+    print("Erro: Entrada inválida. Deve ser inserido números")
+    exit()"""
+
+"""log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+if log['level'] == 'ERROR':
+    print(log['message'])"""
+
+"""idade: int = int(input("Informe a sua idade: "))
+email: str = str(input("Informe seu e-mail: "))
+if not 18 <= idade <= 65:
+    print("Idade fora do intervalo permitido")
+elif "@" not in email or "." not in email:
+    print("Email inválido")
+else:
+    print("Dados de usuário válidos")"""
